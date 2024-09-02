f22594ca1d83e589f1836c0cc4c4eb407b4760ea
snaketron <simo.kitanovski@proton.me>
2024-09-02 21:47:05 +0200

    init working package

---
+++ R/inputs.R
@@ -0,0 +1,192 @@
+
+#' input checks
+#'
+#' The functions takes a data.frame as its main input. It then checks if
+#' the necessary columns are included and processes the input for modeling.
+#'
+#' @return a data.frame
+#' @examples
+#' data(d, package = "cellvel")
+#' pd <- process_input(x = d)
+#' head(pd)
+process_input <- function(x) {
+
+  check_x <- function(x) {
+    if(missing(x)) {
+      stop("missing x")
+    }
+    if(is.data.frame(x)==FALSE) {
+      stop("x must be a data.frame")
+    }
+    if(nrow(x)<=1) {
+      stop("not enough rows in x")
+    }
+  }
+
+  check_cols <- function(x) {
+    if(!"group" %in% colnames(x)) {
+      stop("x does not have a column group")
+    }
+    if(!"replicate" %in% colnames(x)) {
+      stop("x does not have a column replicate")
+    }
+    if(!"batch" %in% colnames(x)) {
+      stop("x does not have a column batch")
+    }
+    if(!"v" %in% colnames(x)) {
+      stop("x does not have a column v")
+    }
+
+    if(is.character(x[,"group"])==FALSE) {
+      stop("column group must be character")
+    }
+    if(is.character(x[,"replicate"])==FALSE) {
+      stop("column replicate must be character")
+    }
+    if(is.character(x[,"batch"])==FALSE) {
+      stop("column batch must be character")
+    }
+    if(is.numeric(x[,"v"])==FALSE) {
+      stop("column v must be numeric")
+    }
+
+    if(any(is.na(x[,"group"]))) {
+      stop("column group contains NAs")
+    }
+    if(any(is.na(x[,"batch"]))) {
+      stop("column batch contains NAs")
+    }
+    if(any(is.na(x[,"replicate"]))) {
+      stop("column replicate contains NAs")
+    }
+    if(any(is.na(x[,"v"]))) {
+      stop("column v contains NAs")
+    }
+  }
+
+  check_x(x=x)
+  check_cols(x=x)
+
+  x$g <- as.numeric(as.factor(x$group))
+  x$r <- as.numeric(as.factor(x$replicate))
+  x$b <- as.numeric(as.factor(x$batch))
+  x$sv <- x$v/max(x$v)
+  x$sample <- paste0(x$batch, '|', x$replicate, '|', x$group)
+  x$s <- as.numeric(as.factor(x$sample))
+  return(x)
+}
+
+
+
+
+process_control <- function(control_in) {
+  control <- list(mcmc_warmup = 500,
+                  mcmc_steps = 1500,
+                  mcmc_chains = 4,
+                  mcmc_cores = 1,
+                  mcmc_algorithm = "NUTS",
+                  adapt_delta = 0.95,
+                  max_treedepth = 12)
+
+  # if missing control_in -> use default values
+  if(missing(control_in) || is.null(control_in)) {
+    return(control)
+  }
+  if(is.list(control_in) == FALSE) {
+    stop("control must be a list")
+  }
+  if(all(names(control_in) %in% names(control)) == FALSE) {
+    stop("unrecognized elements found in control")
+  }
+
+  ns <- names(control_in)
+  for (i in seq_len(length(control_in))) {
+    control[[ns[i]]] <- control_in[[ns[i]]]
+  }
+
+
+  check_mcmc_steps(mcmc_steps = control$mcmc_steps,
+                   mcmc_warmup = control$mcmc_warmup)
+
+  check_mcmc_chains(mcmc_chains = control$mcmc_chains)
+
+  check_mcmc_cores(mcmc_cores = control$mcmc_cores)
+
+  return(control)
+}
+
+
+
+
+
+
+# MCMC Iterations check
+check_mcmc_steps <- function(mcmc_steps,
+                             mcmc_warmup) {
+
+  if(length(mcmc_steps) != 1 |
+     length(mcmc_warmup) != 1) {
+    stop("mcmc_steps >= 500 & mcmc_warmup >= 100.")
+  }
+
+  if(!is.numeric(mcmc_steps) |
+     !is.numeric(mcmc_warmup)) {
+    stop("mcmc_steps >= 500 & mcmc_warmup >= 100.")
+  }
+
+
+  if(is.finite(x = mcmc_steps)==FALSE |
+     is.finite(x = mcmc_warmup)==FALSE) {
+    stop("mcmc_steps >= 500 & mcmc_warmup >= 100.")
+  }
+
+
+  if(as.integer(x = mcmc_steps) < 500 |
+     as.integer(x = mcmc_warmup) < 100) {
+    stop("mcmc_steps >= 500 & mcmc_warmup >= 100.")
+  }
+
+
+  if(as.integer(x = mcmc_steps) <= as.integer(x = mcmc_warmup)) {
+    stop("mcmc_steps > mcmc_warmup")
+  }
+}
+
+# MCMC Chain number check
+check_mcmc_chains <- function(mcmc_chains) {
+  if(length(mcmc_chains) != 1) {
+    stop("mcmc_chains must be a positive integer > 0")
+  }
+
+  if(!is.numeric(mcmc_chains)) {
+    stop("mcmc_chains must be a positive integer > 0")
+  }
+
+  if(is.finite(x = mcmc_chains) == FALSE) {
+    stop("mcmc_chains must be a positive integer > 0")
+  }
+
+  if(as.integer(x = mcmc_chains) <= 0) {
+    stop("mcmc_chains must be a positive integer > 0")
+  }
+}
+
+# MCMC Cores number check
+check_mcmc_cores <- function(mcmc_cores) {
+  if(length(mcmc_cores) != 1) {
+    stop("mcmc_cores must be a positive integer > 0")
+  }
+
+  if(is.numeric(mcmc_cores) == FALSE) {
+    stop("mcmc_cores must be a positive integer > 0")
+  }
+
+  if(is.finite(x = mcmc_cores) == FALSE) {
+    stop("mcmc_cores must be a positive integer > 0")
+  }
+
+  if(as.integer(x = mcmc_cores) <= 0) {
+    stop("mcmc_cores must be a positive integer > 0")
+  }
+}
+
---
+++ R/main.R
@@ -0,0 +1,161 @@
+#' Model-based quantification of cell velocity
+#'
+#' The functions takes a data.frame, x, as its main input. Meanwhile,
+#' the input list control can be used to configure the MCMC procedure
+#' performed by R-package rstan.  The output is a list which contains:
+#' 1) f = fit as rstan object; 2) x = processed input; 3) ppc = plot
+#' with posterior predictive check; 4) s = summary of model parameters
+#' (means, medians, 95% credible intervals, etc.).
+#'
+#' The input x must have cell entries as rows and the following columns:
+#' * replicate = id of the biological replicate (e.g. rep1, rep2, rep3, ...)
+#' * batch = id of the experimental batch (e.g. plate X, plat Y, ...)
+#' * group = treatment group (e.g. compound x at dose 10mmol, ...)
+#' * v = numeric cell speed
+#'
+#' @return a list
+#' @export
+#' @examples
+#' data(d, package = "cellvel")
+#' o <- cellvel(x = d)
+#' head(o)
+cellvel <- function(x, control = NULL) {
+
+  # check inputs
+  x <- process_input(x)
+
+  # check control
+  control <- process_control(control_in = control)
+
+  # fit model
+  f <- get_fit(x = x, control = control)
+
+  # get summary
+  s <- get_summary(x = x, f = f)
+
+  browser()
+  # get ppc plots
+  ppc <- get_ppc(x = x, s = s)
+
+  # get clusters plots
+  # cs <- get_clusters(s = s)
+
+  return(f = f, ppc = ppc, x = x, s = s)
+}
+
+
+get_fit <- function(x, control) {
+  message("model fitting... \n")
+
+  # transform data
+  q <- x[, c("s", "g", "r", "b")]
+  q <- q[duplicated(q)==F, ]
+  q <- q[order(q$s, decreasing = F),]
+
+  # get model
+  M <- stanmodels$M
+
+  # fit model
+  fit <- sampling(object = M,
+                  data = list(y = x$sv, N = nrow(x), s = x$s,
+                              g = q$g, r = q$r, b = q$b),
+                  chains = control$mcmc_chains,
+                  cores = control$mcmc_cores,
+                  iter = control$mcmc_steps,
+                  warmup = control$mcmc_warmup,
+                  algorithm = control$mcmc_algorithm,
+                  control = list(adapt_delta = control$adapt_delta,
+                                 max_treedepth = control$max_treedepth),
+                  refresh = 100)
+
+  return(fit)
+}
+
+
+get_summary <- function(x, f) {
+  message("computing posterior summaries...\n")
+
+  # get unique meta data
+  l <- x[, c("s", "sample", "g", "group", "r", "replicate", "b", "batch")]
+  l_r<-l[duplicated(l[,c("r","replicate")])==FALSE,
+         c("r","replicate")]
+  l_p<-l[duplicated(l[,c("b","batch")])==FALSE,
+         c("b","batch")]
+  l_g<-l[duplicated(l[,c("g","group","r","replicate","b","batch")])==FALSE,
+         c("g","group","r","replicate","b","batch")]
+
+
+  # par: eff_rep
+  eff_rep <- data.frame(summary(f, par = "eff_rep")$summary)
+  eff_rep$r <- 1:nrow(eff_rep)
+  eff_rep <- merge(x = eff_rep, y = l_r, by = "r", all.x = TRUE)
+
+  # par: var_rep
+  var_rep <- data.frame(summary(f, par = "var_rep")$summary)
+  var_rep$r <- 1:nrow(var_rep)
+  var_rep <- merge(x = var_rep, y = l_r, by = "r", all.x = TRUE)
+
+  # par: eff_batch
+  eff_batch <- data.frame(summary(f, par = "eff_batch")$summary)
+  eff_batch$b <- 1:nrow(eff_batch)
+  eff_batch <- merge(x = eff_batch, y = l_p, by = "b", all.x = TRUE)
+
+  # par: var_batch
+  var_batch <- data.frame(summary(f, par = "var_batch")$summary)
+  var_batch$b <- 1:nrow(var_batch)
+  var_batch <- merge(x = var_batch, y = l_p, by = "b", all.x = TRUE)
+
+  # par: eff_group
+  eff_group <- data.frame(summary(f, par = "eff_group")$summary)
+  eff_group$g <- 1:nrow(eff_group)
+  eff_group <- merge(x = eff_group, y = l_g, by = "g", all.x = TRUE)
+
+  # par: var_group
+  var_group <- data.frame(summary(f, par = "var_group")$summary)
+  var_group$g <- 1:nrow(var_group)
+  var_group <- merge(x = var_group, y = l_g, by = "g", all.x = TRUE)
+
+  # par: eff_sigma, var_sigma
+  sigma <- data.frame(summary(f, par = c("eff_sigma", "var_sigma"))$summary)
+
+  # par: mu
+  mu <- data.frame(summary(f, par = "mu")$summary)
+  mu$s <- 1:nrow(mu)
+  mu <- merge(x = mu, y = l, by = "s", all.x = TRUE)
+
+  # par: phi
+  phi <- data.frame(summary(f, par = "phi")$summary)
+  phi$s <- 1:nrow(phi)
+  phi <- merge(x = phi, y = l, by = "s", all.x = TRUE)
+
+
+  # par: y_hat_sample
+  yhat <- data.frame(summary(f, par = "y_hat_sample")$summary)
+  yhat$s <- 1:nrow(yhat)
+  yhat <- merge(x = yhat, y = l, by = "s", all.x = TRUE)
+
+  return(list(eff_rep = eff_rep, eff_batch = eff_batch, eff_group = eff_group,
+              var_rep = var_rep, var_batch = var_batch, var_group = var_group,
+              sigma = sigma, mu = mu, phi = phi, yhat = yhat))
+}
+
+
+get_ppc <- function(x, s) {
+  message("computing ppc...\n")
+
+  g <- ggplot()+
+    geom_sina(data = x, aes(x = as.factor(s), y = sv))+
+    geom_errorbar(data = s$yhat, aes(x = as.factor(s), y = mean,
+                                     ymin = X2.5., ymax = X97.5.),
+                  col = "orange")+
+    theme_bw()+
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+  return(g)
+}
+
+
+get_clusters <- function(x) {
+  message("computing clusters...\n")
+}
+

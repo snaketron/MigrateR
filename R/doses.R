69716e480e622efcbfe97cf93e559ad4fbdb4608
snaketron <simo.kitanovski@uni-due.de>
2025-03-06 14:57:08 +0100

    get_pairs feature

--- R/doses.R
+++ R/doses.R
@@ -60,8 +60,11 @@ get_dose_pmax <- function(x, s, e) {
 
 
 get_pmax <- function(x) {
-  l <- length(x)
-  return(2*max(sum(x<0)/l, sum(x>0)/l)-1)
+    if(all(x==0)) {
+        return(0)
+    }
+    l <- length(x)
+    return(2*max(sum(x<0)/l, sum(x>0)/l)-1)
 }
 
 # Description:
@@ -80,4 +83,53 @@ get_hdi <- function(vec, hdi_level) {
   HDImax = sortedPts[which.min(ciWidth) + ciIdxInc]
   HDIlim = c(HDImin, HDImax)
   return(HDIlim)
+}
+
+
+get_pairs <- function(x) {
+    p <- extract(x$f, par = "mu_group")$mu_group
+    if(ncol(p)==1) {
+        stop("only one treatment group: nothing to compare")
+    }
+    
+    ds <- vector(mode = "list", length = ncol(p)*ncol(p))
+    ct <- 1
+    for(i in 1:ncol(p)) {
+        for(j in 1:ncol(p)) {
+            d <- p[,i]-p[,j]
+            d_M <- mean(d)
+            d_HDI <- get_hdi(vec = d, hdi_level = 0.95)
+            pmax <- get_pmax(d)
+            ds[[ct]] <- data.frame(group_id_x = i, 
+                                   group_id_y = j, 
+                                   delta_M = d_M,
+                                   delta_L95 = d_HDI[1],
+                                   delta_H95 = d_HDI[2],
+                                   pmax = pmax)
+            ct <- ct + 1
+        }
+    }
+    ds <- do.call(rbind, ds)
+    
+    ds <- merge(x = ds, y = x$s$mu_group[, c("group_id", "group")],
+                    by.x = "group_id_x", by.y = "group_id")
+    ds <- merge(x = ds, y = x$s$mu_group[, c("group_id", "group")],
+                    by.x = "group_id_y", by.y = "group_id")
+    colnames(ds) <- gsub(pattern = "\\.", replacement = '_', x = colnames(ds))
+    
+    g <- ggplot(data = ds)+
+        geom_tile(aes(x = group_x, y = group_y), col = "white", fill = "#eeeeee")+
+        geom_point(aes(x = group_x, y = group_y, size = pmax, col = delta_M))+
+        geom_text(aes(x = group_x, y = group_y, 
+                      label = round(x = pmax, digits = 2)), size = 2)+
+        scale_color_distiller(name = expression(delta), palette = "Spectral")+
+        scale_radius(name = expression(pi), limits = c(0, 1))+
+        theme_bw(base_size = 10)+
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+        xlab(label = '')+
+        ylab(label = '')
+    
+    g
+    
+    return(list(ds = ds, plot = g))
 }
